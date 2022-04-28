fa7ba6576a6538024d7337dfacd1c5165691b447
sam-drumm <samueldrumm@gmail.com>
2022-04-28 12:05:07 +1200

    Project styling

--- client/components/Countdown/CountdownTimer.jsx
+++ client/components/Countdown/CountdownTimer.jsx
@@ -1,18 +1,32 @@
+import { HStack, Stack } from '@chakra-ui/react'
 import React from 'react'
 import DateTimeDisplay from './DateTimeDisplay'
 import { useCountdown } from './useCountdown'
 
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
-    <div className="show-counter">
-      <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
-      <p>:</p>
-      <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-      <p>:</p>
-      <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-      <p>:</p>
-      <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-    </div>
+    <Stack
+      spacing={8}
+      p={8}
+      direction={['column', 'row']}
+      borderWidth={2}
+      borderRadius={8}
+      // boxShadow="lg"
+      >
+
+      <h2 m={3}>Project pitch will be live for another:</h2>
+
+      <HStack>
+        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
+      </HStack>
+
+    </Stack>
   )
 }
 
@@ -36,8 +50,8 @@ const CountdownTimer = ({ targetDate }) => {
 const ExpiredNotice = () => {
   return (
     <div className="expired-notice">
-      <span>Expired!!!</span>
-      <p>Please select a future date and time.</p>
+      <span>Project pitch ended!</span>
+      <p>Checkout other great ideas here.</p>
     </div>
   )
 }
--- client/components/Project/Project.jsx
+++ client/components/Project/Project.jsx
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import CountdownTimer from '../Countdown/CountdownTimer'
 import { fetchProject } from '../../actions/project'
-import { Box, Heading, Flex, Button } from '@chakra-ui/react'
+import { Box, Heading, Flex, Button, HStack, ButtonGroup, Stack } from '@chakra-ui/react'
+import { FaFacebook, FaGithub, FaTwitter } from 'react-icons/fa'
 
 export default function Project (props) {
   const dispatch = useDispatch()
@@ -25,11 +26,8 @@ export default function Project (props) {
   } = project
 
   const fourteenDaysMS = 14 * 24 * 60 * 60 * 1000
-  const timeMS = new Date().getTime()
-  const createdMS = project.createdAt.getTime()
+  const createdMS = new Date(createdAt).getTime()
   const expiryMS = createdMS + fourteenDaysMS
-
-
 
   useEffect(() => {
     dispatch(fetchProject(id, token))
@@ -37,31 +35,64 @@ export default function Project (props) {
 
   return (
 
-    <Flex width="full" align="center" justifyContent="center" marginTop={10} marginBottom={10}>
+    <Flex width="full" align="center" justifyContent="center" marginTop={10} marginBottom={10} padding={10}
+    >
       <Box
         p={8}
-        maxWidth="500px"
+        maxWidth="750px"
         borderWidth={2}
         borderRadius={8}
         boxShadow="lg"
       >
 
-        <Box textAlign="centre">
+        <Box
+          textAlign="centre"
+          width="full"
+          alignContent="center"
+          // marginTop={10}
+          // borderRadius={8}
+
+          marginBottom={10}
+          // borderWidth={2}
+          // boxShadow='2xl'
+        >
           <Heading>{projectTitle}</Heading>
+          <p>{region}</p>
+          <p>{category}</p>
+          <p>{description}</p>
+          <p>{seeking}</p>
+          <p>{purpose}</p>
+          <p>{teamEstablished}</p>
+          <p>{started}</p>
+          <p>{skillType}</p>
+          <p>{skillDescription}</p>
+          <p>{createdAt}</p>
         </Box>
-        <p>{region}</p>
-        <p>{category}</p>
-        <p>{description}</p>
-        <p>{seeking}</p>
-        <p>{purpose}</p>
-        <p>{teamEstablished}</p>
-        <p>{started}</p>
-        <p>{skillType}</p>
-        <p>{skillDescription}</p>
-        <Button m={6}>
+        {/* <Box> */}
+        <CountdownTimer targetDate={expiryMS}/>
+        {/* </Box> */}
+        <Button
+          size='lg'
+          width="full"
+          alignContent="center"
+          marginTop={10}
+          marginBottom={10}
+          borderWidth={2}
+          boxShadow='2xl'
+        >
           <p>Count me in!</p>
         </Button>
-        <CountdownTimer targetDate={expiryMS} />
+        <HStack spacing="auto">
+          <Button colorScheme='facebook' leftIcon={<FaFacebook />}>
+    Facebook
+          </Button>
+          <Button colorScheme='twitter' leftIcon={<FaTwitter />}>
+    Twitter
+          </Button>
+          <Button colorScheme='blackAlpha' leftIcon={<FaGithub />}>
+    GitHub
+          </Button>
+        </HStack>
 
       </Box>
     </Flex>
