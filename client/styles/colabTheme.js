2e8dd5dd2a5a9cd35bbe269e330389ab47c3689d
sam-drumm <samueldrumm@gmail.com>
2022-06-07 10:55:07 +0100

    restructed directory for deployment

--- client/styles/colabTheme.js
+++ client/styles/colabTheme.js
@@ -1,10 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
-import { StepsStyleConfig as Steps } from 'chakra-ui-steps'
+// import { StepsStyleConfig as Steps } from 'chakra-ui-steps'
 
 const colabTheme = extendTheme({
-  components: {
-    Steps
-  },
+  // components: {
+  //   Steps
+  // },
   breakpoints: {
     sm: '30em',
     md: '48em',
