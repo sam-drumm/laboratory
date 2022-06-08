3d956f7423e05bd10222c8f46afd3206a29a0a14
sam-drumm <samueldrumm@gmail.com>
2022-06-09 00:40:12 +0100

    messages refs updated

--- server/db/migrations/20220524153829_messages.js
+++ server/db/migrations/20220524153829_messages.js
@@ -1,14 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable('messages', table => {
     table.increments('id').primary()
-    table.string('project_id').nullable()
-    table.string('auth0_id').nullable()
     table.string('why')
     table.string('bring')
     table.string('share')
-
-    table.foreign('project_id').references('projects.id')
-    table.foreign('auth0_id').references('users.auth0_id')
+    table.integer('project_id').references('projects.id')
+    table.string('auth0_id').references('users.auth0_id')
   })
 }
 
