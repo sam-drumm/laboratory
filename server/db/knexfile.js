618e0081f7c1d83f076cac25ee89fc0d9a624bd1
sam-drumm <samueldrumm@gmail.com>
2022-06-23 22:03:16 +0100

    switch to postgres local

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,24 +1,21 @@
 const path = require('path')
 
 module.exports = {
+
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: path.join(__dirname, 'dev.sqlite3')
+    client: 'pg',
+    connection: 'postgres://localhost/colab',
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
+    },
+    migrations: {
+      directory: path.join(__dirname, 'migrations')
     },
     useNullAsDefault: true
   },
 
-  // test: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: ':memory:'
-  //   },
-  //   useNullAsDefault: true
-  // },
-
   test: {
-    client: 'sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
     connection: {
       filename: ':memory:'
@@ -31,21 +28,8 @@ module.exports = {
     }
   },
 
-  //   production: {
-  //     client: 'postgresql',
-  //     connection: process.env.DATABASE_URL,
-  //     pool: {
-  //       min: 2,
-  //       max: 10
-  //     },
-  //     migrations: {
-  //       tableName: 'knex_migrations'
-  //     }
-  //   }
-  // }
-
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
